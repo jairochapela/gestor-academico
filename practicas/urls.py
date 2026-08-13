06492f1d7d5643a6e412e9c5f20b7cfaf84f7bc7
Jairo Chapela <jairochapela@gmail.com>
2026-08-13 13:17:41 +0200

    Rutas pendientes

--- practicas/urls.py
+++ practicas/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
-    
+    path('empresas/', views.list_empresas, name='list_empresas'),    
     path('empresas/<int:id>/', views.empresa_detalle, name='empresa_detalle'),  
-    path('empresas/', views.list_empresas, name='list_empresas'),
     path('alumnos/', views.lista_alumno, name='lista_alumno'),
     path('alumnos/<int:pk>/', views.AlumnoDetailView.as_view(), name='alumno_detalle'),
+    # TODO path('cursos/', views.lista_curso, name='lista_curso'),
+    # TODO path('cursos/<int:pk>/', views.CursoDetailView.as_view(), name='curso_detalle'),
 ]
