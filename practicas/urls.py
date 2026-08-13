51c173155c78a32a67f59490827cbc27f729c982
Jairo Chapela <jairochapela@gmail.com>
2026-08-13 13:15:55 +0200

    Rutas de listados de empresas y alumnos

--- practicas/urls.py
+++ practicas/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.frontpage, name='frontpage'),
     
     path('empresas/<int:id>/', views.empresa_detalle, name='empresa_detalle'),  
+    path('empresas/', views.list_empresas, name='list_empresas'),
+    path('alumnos/', views.lista_alumno, name='lista_alumno'),
     path('alumnos/<int:pk>/', views.AlumnoDetailView.as_view(), name='alumno_detalle'),
 ]
