6ac2d4967d29429d78eb30e1c4d88b08ee48788f
Alguien <algo@correo.com>
2026-08-13 11:28:34 +0200

    api urls y urls separadas

--- practicas/urls.py
+++ practicas/urls.py
@@ -1,28 +1,12 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 
 from . import views
-from practicas.views import (
-    AlumnoListView,
-    CursoViewSet,
-    EmpresaViewSet,
-    HorarioViewSet,
-    AlumnoViewSet,
-    list_empresas,
-)
-
-router = routers.DefaultRouter()
-router.register(r'cursos', CursoViewSet)
-router.register(r'empresas', EmpresaViewSet)
-router.register(r'horarios', HorarioViewSet)
-router.register(r'alumnos', AlumnoViewSet)
 
 urlpatterns = [
-    path('empresas/', list_empresas, name='list_empresas'),
-    path('', include(router.urls)),
-    path('alumnos/', AlumnoListView.as_view(), name='listado_alumnos'),
-
-
+    # Portada
     path('', views.frontpage, name='frontpage'),
-    path('api/v1/', include(router.urls)),
+
+    # Vistas HTML
+    path('empresas/lista/', views.list_empresas, name='list_empresas'),
+    path('alumnos/lista/', views.AlumnoListView.as_view(), name='listado_alumnos'),
 ]
